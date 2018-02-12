3f2192a05c87be59ec21fb53622ea55e2f234381
sandypants93 <sandypants93@gmail.com>
2018-02-12 09:29:14 -0500

    Modify role.spawner.js -removed road building because cancerous

--- role.spawner.js
+++ role.spawner.js
@@ -82,29 +82,7 @@ var roleSpawner = {
                 {align : 'center', opacity : 0.8});
         }
         /* automated construction */
-        if(spawn.memory.building == 0) {
-            var sources = spawn.room.find(FIND_SOURCES);
-            var con = spawn.room.controller;
-            var plan = Array(), len = sources.length;
-            var max
-            while (len--) {
-                for(var tile in spawn.pos.findPathTo(sources[len])) {
-                    plan.push(String(tile.x) + '|' + String(tile.y));
-                }
-                for(var tile in con.pos.findPathTo(sources[len])) {
-                    plan.push(String(tile.x) + '|' + String(tile.y));
-                }
-            }
-            var sites = spawn.room.lookAtArea(0, 0, 49, 49, true, {
-                filter: (site) => (String(site.x) + '|' + String(site.y)) in plan && site.type == 'terrain'});
-            if(sites.length) {
-                spawn.room.createConstructionSite(sites[0].x, sites[0].y, STRUCTURE_ROAD);
-            }
-            else {
-                spawn.memory.building++;
-            }
-        }
-        if(spawn.memory.building == 1 && conLvl > 1) {
+        if(spawn.memory.building == 0 && conLvl > 1) {
             var sites = spawn.room.lookAtArea((spawn.pos.y - 10), (spawn.pos.x - 10), (spawn.pos.y + 10), (spawn.pos.x + 10), true);
             var check = _.filter(sites, (info) => info.terrain == 'wall' || info.type != 'terrain');
             var bad = Array();
@@ -123,7 +101,7 @@ var roleSpawner = {
                 spawn.memory.building++;
             }
         }
-        else if(spawn.memory.building == 2 && conLvl > 1) {
+        else if(spawn.memory.building == 1 && conLvl > 1) {
             var ex_con = spawn.room.find(FIND_MY_CONSTRUCTION_SITES, {filter: (site) => site.structureType == STRUCTURE_EXTENSION});
             var ex_str = spawn.room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_EXTENSION});
             if(ex_con.length + ex_str.length < 5) {
@@ -146,7 +124,7 @@ var roleSpawner = {
                 spawn.memory.building++;
             }
         }
-        else if(spawn.memory.building == 3 && conLvl > 2) {
+        else if(spawn.memory.building == 2 && conLvl > 2) {
             var tow_str = spawn.room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_TOWER});
             var tow_con = spawn.room.find(FIND_MY_CONSTRUCTION_SITES, {filter: (site) => site.structureType == STRUCTURE_TOWER});
             if(tow_str.length + tow_con.length < 1) {
@@ -156,7 +134,7 @@ var roleSpawner = {
                 spawn.memory.building++;
             }
         } 
-        else if(spawn.memory.building == 4 && conLvl > 3) {
+        else if(spawn.memory.building == 3 && conLvl > 3) {
             var tow_str = spawn.room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_TOWER});
             var tow_con = spawn.room.find(FIND_MY_CONSTRUCTION_SITES, {filter: (site) => site.structureType == STRUCTURE_TOWER});
             if(tow_str.length + tow_con.length < 2) {
@@ -166,7 +144,7 @@ var roleSpawner = {
                 spawn.memory.building++;
             }
         }
-        else if(spawn.memory.building == 5 && conLvl > 6) {
+        else if(spawn.memory.building == 4 && conLvl > 6) {
             var tow_str = spawn.room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_TOWER});
             var tow_con = spawn.room.find(FIND_MY_CONSTRUCTION_SITES, {filter: (site) => site.structureType == STRUCTURE_TOWER});
             if(tow_str.length + tow_con.length < 2) {
@@ -176,7 +154,7 @@ var roleSpawner = {
                 spawn.memory.building++;
             }
         }
-        else if(spawn.memory.building == 6 && conLvl > 7) {
+        else if(spawn.memory.building == 5 && conLvl > 7) {
             var tow_str = spawn.room.find(FIND_MY_STRUCTURES, {filter: (structure) => structure.structureType == STRUCTURE_TOWER});
             var tow_con = spawn.room.find(FIND_MY_CONSTRUCTION_SITES, {filter: (site) => site.structureType == STRUCTURE_TOWER});
             if(tow_str.length + tow_con.length < 2) {
